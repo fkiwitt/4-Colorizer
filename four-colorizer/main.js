66dc92baeda5734bb01bf82c4dc89a7e0370e8f0
Fynn Kiwitt <fynnkiwitt@yahoo.de>
2021-03-05 00:37:38 +0100

    basic drawing

---
+++ four-colorizer/main.js
@@ -0,0 +1,54 @@
+//// Painting
+//Canvas
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+//Variables
+var canvasx = $(canvas).offset().left;
+var canvasy = $(canvas).offset().top;
+var last_mousex = last_mousey = 0;
+var mousex = mousey = 0;
+var mousedown = false;
+
+var stroke_history = [];
+var current_stroke = [0, 0, 0, 0];
+
+function draw_current_canvas(){
+	for(stroke in stroke_history){
+		draw_strokes(stroke_history[stroke]);
+	}
+	draw_strokes(current_stroke);
+}
+
+function draw_strokes(stroke){
+	ctx.beginPath();
+	ctx.moveTo(stroke[0], stroke[1]);
+	ctx.lineTo(stroke[2], stroke[3]);
+	ctx.strokeStyle = 'black';
+	ctx.lineWidth = 10;
+	ctx.lineJoin = ctx.lineCap = 'round';
+	ctx.stroke();
+}
+
+//Mousedown
+$(canvas).on('mousedown', function(e) {
+    start_x = parseInt(e.clientX-canvasx);
+		start_y = parseInt(e.clientY-canvasy);
+    mousedown = true;
+});
+
+//Mouseup
+$(canvas).on('mouseup', function(e) {
+    mousedown = false;
+		stroke_history.push(current_stroke);
+});
+
+//Mousemove
+$(canvas).on('mousemove', function(e) {
+    mouse_x = parseInt(e.clientX-canvasx);
+		mouse_y = parseInt(e.clientY-canvasy);
+    if(mousedown) {
+        ctx.clearRect(0,0,canvas.width,canvas.height); //clear canvas
+				current_stroke = [start_x, start_y, mouse_x, mouse_y];
+				draw_current_canvas();
+    }
+});
